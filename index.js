308feea2f43e6d013d6f2956a3cd34da2c1f084e
james59988 <james58899@oktw.tw>
2016-07-30 11:17:18 +0000

    clean code

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const cheerio = require('cheerio');
-const request = require('request');
-const fs = require('fs');
-const util = require('util');
-const moment = require('moment');
-const schedule = require('node-schedule');
-const TelegramBot = require('node-telegram-bot-api');
+const cheerio = require('cheerio'),
+    fs = require('fs'),
+    util = require('util'),
+    moment = require('moment'),
+    schedule = require('node-schedule'),
+    TelegramBot = require('node-telegram-bot-api'),
+    request = require("request");
 
-var username;
-
+var username, updateTime;
 var pubDate = moment();
 var config = JSON.parse(fs.readFileSync('data.json', 'utf8'));
 var bot = new TelegramBot(config.key, {
@@ -25,13 +24,15 @@ bot.onText(/^\/(\w+)@?\w*/i, function(msg, regex) {
     if (msg.chat.type === 'private') {
         console.log('%s(%s) => %s: %s', msg.from.username, msg.from.id, username, msg.text);
     }
-    else {
-        console.log('%s(%s) => %s(%s): %s', msg.from.username, msg.from.id, msg.chat.title, msg.chat.id, msg.text);
-    }
+    else console.log('%s(%s) => %s(%s): %s', msg.from.username, msg.from.id, msg.chat.title, msg.chat.id, msg.text);
+
 
     switch (regex[1]) {
         case 'update':
-            getUpdate();
+            if (!updateTime || updateTime.add(10, 'm').isBefore(moment())) {
+                getUpdate();
+                updateTime = moment();
+            }
             break;
         case 'search':
             search(msg);
@@ -43,9 +44,8 @@ bot.onText(/^\/(\w+)@?\w*/i, function(msg, regex) {
                 console.log(msg.chat.id + ' Add to subscription list.');
                 bot.sendMessage(msg.chat.id, '已加入訂閱清單！');
             }
-            else {
-                bot.sendMessage(msg.chat.id, '已經在訂閱清單中了！');
-            }
+            else bot.sendMessage(msg.chat.id, '已經在訂閱清單中了！');
+
             break;
         case 'unsubscribe':
             if (config.channel.indexOf(msg.chat.id) > -1) {
@@ -54,9 +54,8 @@ bot.onText(/^\/(\w+)@?\w*/i, function(msg, regex) {
                 console.log(msg.chat.id + ' remove from subscription list.');
                 bot.sendMessage(msg.chat.id, '已從訂閱清單中刪除');
             }
-            else {
-                bot.sendMessage(msg.chat.id, '尚未訂閱');
-            }
+            else bot.sendMessage(msg.chat.id, '尚未訂閱');
+
             break;
     }
 });
@@ -83,93 +82,92 @@ bot.on('left_chat_participant', function(msg) {
 
 var search = function(msg) {
     var keyword = msg.text.match(/\s(.+)/);
-    if (keyword) {
-        keyword = encodeURI(keyword[1].replace(' ', '+'));
-        request('https://share.dmhy.org/topics/rss/sort_id/31/rss.xml?keyword=' + keyword, function(err, res, body) {
-            if (!err && res.statusCode == 200) {
-                var result = [];
-                var $ = cheerio.load(body, {
-                    xmlMode: true
-                });
+    if (!keyword) {
+        bot.sendMessage(msg.chat.id, '請輸入要搜尋的關鍵字！\n範例：/search 果 青\n群組內僅顯示五個結果\n更多資訊請看 https://share.dmhy.org/cms/page/name/faq.html#faq3');
+        return;
+    }
 
-                if ($("item").index() > 0) {
-                    result.push('目前搜尋範圍：季度全集');
-                    $("item").each(function(i, elem) {
-                        if (i < 5) {
-                            result.push(util.format('<a href="%s">%s</a>', $(this).children('link').text(), $(this).children('title').text()));
-                        }
-                    });
-                    bot.sendMessage(msg.chat.id, result.join('\n\n'), {
-                        parse_mode: 'HTML',
-                        disable_web_page_preview: true,
-                        disable_notification: true
-                    });
+    keyword = encodeURI(keyword[1].replace(' ', '+'));
+    request('https://share.dmhy.org/topics/rss/sort_id/31/rss.xml?keyword=' + keyword, function(err, res, body) {
+        if (err || res.statusCode != 200) {
+            bot.sendMessage(msg.chat.id, '抓取結果時發生錯誤！');
+            console.error(err);
+            return;
+        }
+        var processItem = function() {
+            $("item").each(function(i, elem) {
+                if (msg.chat.type == 'private') {
+                    result.push(util.format('<a href="%s">%s</a>', $(this).children('link').text(), $(this).children('title').text()));
                 }
-                else {
-                    request('http://share.dmhy.org/topics/rss/rss.xml?keyword=' + keyword, function(err, res, body) {
-                        if (!err && res.statusCode == 200) {
-                            var result = [];
-                            var $ = cheerio.load(body, {
-                                xmlMode: true
-                            });
+                else if (i < 5) {
+                    result.push(util.format('<a href="%s">%s</a>', $(this).children('link').text(), $(this).children('title').text()));
+                }
+            });
+            bot.sendMessage(msg.chat.id, result.join('\n\n'), {
+                parse_mode: 'HTML',
+                disable_web_page_preview: true,
+                disable_notification: true
+            });
+        };
 
-                            if ($("item").index() > 0) {
-                                result.push('目前搜尋範圍：全部');
-                                $("item").each(function(i, elem) {
-                                    if (i < 5) {
-                                        result.push(util.format('<a href="%s">%s</a>', $(this).children('link').text(), $(this).children('title').text()));
-                                    }
-                                });
-                                bot.sendMessage(msg.chat.id, result.join('\n\n'), {
-                                    parse_mode: 'HTML',
-                                    disable_web_page_preview: true,
-                                    disable_notification: true
-                                });
-                            }
-                            else {
-                                bot.sendMessage(msg.chat.id, '找不到任何結果！');
-                            }
-                        }
-                    });
-                }
-            }
+        var result = [];
+        var $ = cheerio.load(body, {
+            xmlMode: true
         });
-    }
-    else {
-        bot.sendMessage(msg.chat.id, '請輸入要搜尋的關鍵字！\n範例：/search 果 青\n更多資訊請看 https://share.dmhy.org/cms/page/name/faq.html#faq3');
-    }
+        if ($("item").index() > 0) {
+            result.push('目前搜尋範圍：季度全集');
+            processItem();
+        }
+        else request('http://share.dmhy.org/topics/rss/rss.xml?keyword=' + keyword, function(err, res, body) {
+            if (err || res.statusCode != 200) {
+                bot.sendMessage(msg.chat.id, '抓取結果時發生錯誤！');
+                console.error(err);
+                return;
+            }
+
+            $ = cheerio.load(body, {
+                xmlMode: true
+            });
+            if ($("item").index() > 0) {
+                result.push('目前搜尋範圍：全部');
+                processItem();
+            }
+            else bot.sendMessage(msg.chat.id, '找不到任何結果！');
+        });
+    });
 };
 
 var getUpdate = function() {
-    request('https://share.dmhy.org/topics/rss/rss.xml?keyword=%E7%B9%81%7Cbig5%7Ccht&sort_id=2', function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var messages = [];
-            var tmpDate;
-            var $ = cheerio.load(body, {
-                xmlMode: true
-            });
+    request('https://share.dmhy.org/topics/rss/rss.xml?keyword=%E7%B9%81%7Cbig5%7Ccht&sort_id=2', function(err, res, body) {
+        if (err || res.statusCode != 200) {
+            return;
+        }
+        var messages = [];
+        var tmpDate;
+        var $ = cheerio.load(body, {
+            xmlMode: true
+        });
 
-            $("item").each(function(i, elem) {
-                var date = moment($(this).children('pubDate').text(), 'ddd, DD MMM YYYY HH:mm:ss ZZ');
-                if (pubDate.isBefore(date)) {
-                    messages.push(util.format('<a href="%s">%s</a>', $(this).children('link').text(), $(this).children('title').text()));
-                    if (!tmpDate) tmpDate = date;
-                }
-            });
-            if (tmpDate) pubDate = tmpDate;
-
-            console.log('fetch %s updates', messages.length);
-            if (messages.length > 0) {
-                messages.reverse();
-                config.channel.forEach(function(channel) {
-                    console.log('Send Update to %s', channel);
-                    bot.sendMessage(channel, messages.join('\n\n'), {
-                        parse_mode: 'HTML',
-                        disable_web_page_preview: true,
-                        disable_notification: true
-                    });
-                });
+        $("item").each(function(i, elem) {
+            var date = moment($(this).children('pubDate').text(), 'ddd, DD MMM YYYY HH:mm:ss ZZ');
+            if (pubDate.isBefore(date)) {
+                messages.push(util.format('<a href="%s">%s</a>', $(this).children('link').text(), $(this).children('title').text()));
+                if (!tmpDate) tmpDate = date;
             }
+        });
+        if (tmpDate) pubDate = tmpDate;
+
+        console.log('fetch %s updates', messages.length);
+        if (messages.length > 0) {
+            messages.reverse();
+            config.channel.forEach(function(channel) {
+                console.log('Send Update to %s', channel);
+                bot.sendMessage(channel, messages.join('\n\n'), {
+                    parse_mode: 'HTML',
+                    disable_web_page_preview: true,
+                    disable_notification: true
+                });
+            });
         }
     });
 };
